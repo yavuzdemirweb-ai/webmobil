1129e469fbacd459f45585f42a1aa52e895bf52c
yavuzdemirweb-ai <yavuzdemirweb@gmail.com>
2026-09-25 04:49:22 +0300

    sw-early-register

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('SW kaydedildi:', registration.scope);
-    }).catch((error) => {
-      console.log('SW kaydi basarisiz:', error);
-    });
+  navigator.serviceWorker.register('/sw.js').then((registration) => {
+    console.log('SW kaydedildi:', registration.scope);
+  }).catch((error) => {
+    console.log('SW kaydi basarisiz:', error);
   });
 }
 
